d0055a57e1e09afac769751367937542c8ed3b9d
schmodd <schmodd@enexo.de>
2013-04-01 11:55:34 +0200

    convert tabs to spaces (indentations)

--- twitchscript.py
+++ twitchscript.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
-""" 
+"""
 
 Twitchscript is a little Python Script for Linux, enabling you to start Twitch Streams from the CLI.
-It basically provides a menu so that u dont need to browse Twitch in order to get the current streams. 
-I mainly use it for watching streams with the Raspberry Pi which is connected to my TV (via HDMI). 
+It basically provides a menu so that u dont need to browse Twitch in order to get the current streams.
+I mainly use it for watching streams with the Raspberry Pi which is connected to my TV (via HDMI).
 However you can also use it on a usual machine with vlc.
 
 I am new to python so feel free to criticize me.
 Hints and suggestions are appreciated.
 
-Requirements: 
+Requirements:
 - livestreamer must be installed (https://github.com/chrippa/livestreamer) -> easy install via "python-pip" (pip install livestreamer)
 - make this script executable (chmod +x) and run it!
-- you may have to adapt some settings regarding your environment 
+- you may have to adapt some settings regarding your environment
 
 Todo:
 - input validation
@@ -41,116 +41,116 @@ games = []
 channels = []
 
 def getTwitchApiRequestStreams(limit, game):
-	#print 'using: ' + twitchApiUrl + 'streams?limit=' + str(limit) + '&game=' + game
-	return urllib2.urlopen((twitchApiUrl + 'streams?limit=' + str(limit) + '&game=' + game).encode('UTF-8')).read()
+    #print 'using: ' + twitchApiUrl + 'streams?limit=' + str(limit) + '&game=' + game
+    return urllib2.urlopen((twitchApiUrl + 'streams?limit=' + str(limit) + '&game=' + game).encode('UTF-8')).read()
 
 def getTwitchApiRequestGames(limit):
-	#print 'using: ' + twitchApiUrl + 'games/top?limit=' + str(limit)
-	return urllib2.urlopen(twitchApiUrl + 'games/top?limit=' + str(limit)).read()
+    #print 'using: ' + twitchApiUrl + 'games/top?limit=' + str(limit)
+    return urllib2.urlopen(twitchApiUrl + 'games/top?limit=' + str(limit)).read()
 
 def getChannels(game):
-	global channelLimit
+    global channelLimit
 
-	channeldict = json.loads(getTwitchApiRequestStreams(channelLimit, game))
+    channeldict = json.loads(getTwitchApiRequestStreams(channelLimit, game))
 
-	if channelLimit > len(channeldict['streams']):
-		channelLimit = len(channeldict['streams'])
-		print 'Only ' + str(channelLimit) + ' channels available!'
+    if channelLimit > len(channeldict['streams']):
+        channelLimit = len(channeldict['streams'])
+        print 'Only ' + str(channelLimit) + ' channels available!'
 
-	for i in range(channelLimit):
-		channels.append(channeldict['streams'][i]['channel']['name'])
+    for i in range(channelLimit):
+        channels.append(channeldict['streams'][i]['channel']['name'])
 
 def showChannels():
-	for i in range(len(channels)):
-		if i < 9:
-			print ' %d %s' % (i + 1, channels[i])
-		else:	
-			print '%d %s' % (i + 1, channels[i])
+    for i in range(len(channels)):
+        if i < 9:
+            print ' %d %s' % (i + 1, channels[i])
+        else:
+            print '%d %s' % (i + 1, channels[i])
 
 def getGames():
-	global gameLimit
+    global gameLimit
 
-	gamesDict = json.loads(getTwitchApiRequestGames(gameLimit))
+    gamesDict = json.loads(getTwitchApiRequestGames(gameLimit))
 
-	if gameLimit > len(gamesDict['top']):
-		gameLimit = len(gamesDict['top'])
-		print 'Only ' + str(gameLimit) + ' games available!'
+    if gameLimit > len(gamesDict['top']):
+        gameLimit = len(gamesDict['top'])
+        print 'Only ' + str(gameLimit) + ' games available!'
 
-	for i in range(gameLimit):
-		games.append(gamesDict['top'][i]['game']['name'])
+    for i in range(gameLimit):
+        games.append(gamesDict['top'][i]['game']['name'])
 
 def showGames():
-	for i in range(len(games)):
-		if i < 9:
-			print ' %d %s' % (i + 1, games[i])
-		else:
-			print '%d %s' % (i + 1, games[i])
+    for i in range(len(games)):
+        if i < 9:
+            print ' %d %s' % (i + 1, games[i])
+        else:
+            print '%d %s' % (i + 1, games[i])
 
 def playStream(channel):
-	if usingOmxPlayer:
-		os.system('livestreamer twitch.tv/' + channel + ' best -np "omxplayer -o hdmi"')
-	else:
-		os.system('livestreamer twitch.tv/' + channel + ' best')
+    if usingOmxPlayer:
+        os.system('livestreamer twitch.tv/' + channel + ' best -np "omxplayer -o hdmi"')
+    else:
+        os.system('livestreamer twitch.tv/' + channel + ' best')
 
 def reset():
-	channels[:] = []
-	games[:] = []
+    channels[:] = []
+    games[:] = []
 
 def transformSpaces(stringToTransform):
-	return stringToTransform.replace(' ', '%20')
+    return stringToTransform.replace(' ', '%20')
 
 def clearScreen():
-	os.system('clear')
+    os.system('clear')
 
 def getUserInput(message, validValues):
-	
-	userInputValid = False
-	
-	while not userInputValid:
-		try:
-			choice = int(raw_input('%s\n> ' % message))
-			if choice in validValues:
-				userInputValid = True
-			else: 
-				raise ValueError()
-		except ValueError:
-			print 'Wrong Input! Please enter valid Values!'
-	return choice
+
+    userInputValid = False
+
+    while not userInputValid:
+        try:
+            choice = int(raw_input('%s\n> ' % message))
+            if choice in validValues:
+                userInputValid = True
+            else:
+                raise ValueError()
+        except ValueError:
+            print 'Wrong Input! Please enter valid Values!'
+    return choice
 
 def run():
-	while True:
-		clearScreen()
-		print '\nLoading'
-		getGames()
-		clearScreen()
-		print '\nWelcome to twitchscript!'
-		print '\nShowing top %s games:' % str(gameLimit)
-		print '------------------------------------------------------------'
-		showGames()
-		
-		chosenGame = getUserInput('\nChoose game by number (0 for exit):', range(gameLimit + 1))
+    while True:
+        clearScreen()
+        print '\nLoading'
+        getGames()
+        clearScreen()
+        print '\nWelcome to twitchscript!'
+        print '\nShowing top %s games:' % str(gameLimit)
+        print '------------------------------------------------------------'
+        showGames()
 
-		clearScreen()
-		if int(chosenGame) in range(0, len(games) + 1):
-			if int(chosenGame) is 0:
-				sys.exit(0)
-			print '\nLoading channellist: "%s"\n' % games[int(chosenGame) - 1]
-			getChannels(transformSpaces(games[int(chosenGame) - 1]))
+        chosenGame = getUserInput('\nChoose game by number (0 for exit):', range(gameLimit + 1))
 
-		showChannels()
+        clearScreen()
+        if int(chosenGame) in range(0, len(games) + 1):
+            if int(chosenGame) is 0:
+                sys.exit(0)
+            print '\nLoading channellist: "%s"\n' % games[int(chosenGame) - 1]
+            getChannels(transformSpaces(games[int(chosenGame) - 1]))
 
-		chosenChannel = getUserInput('\nChoose channel by number (0 for exit):', range(channelLimit + 1))
-		
-		clearScreen()
-		if int(chosenChannel) in range(0, len(channels) + 1):
-			if int(chosenChannel) is 0:
-				sys.exit(0)
-			print '\nLoading stream: "%s"\n' % channels[int(chosenChannel) - 1]
-			playStream(channels[int(chosenChannel) - 1])
-		reset()
+        showChannels()
+
+        chosenChannel = getUserInput('\nChoose channel by number (0 for exit):', range(channelLimit + 1))
+
+        clearScreen()
+        if int(chosenChannel) in range(0, len(channels) + 1):
+            if int(chosenChannel) is 0:
+                sys.exit(0)
+            print '\nLoading stream: "%s"\n' % channels[int(chosenChannel) - 1]
+            playStream(channels[int(chosenChannel) - 1])
+        reset()
 
 def main():
-	run()
+    run()
 
 if __name__ == '__main__':
     main()
